11e4ad280590849b3cef7aff111a573ef2dbe2ad
Owen Chaffard <owenchaff@gmail.com>
2021-10-15 20:11:17 +0200

    chore: adding timestamp to share

--- classes/share.py
+++ classes/share.py
@@ -1,13 +1,16 @@
 class Share ():
     id: str
     owner: str
+    time: int
 
-    def __init__(self, id, owner):
+    def __init__(self, id, owner,time):
         self.id = id
         self.owner = owner
+        self.time = time
 
     def fromJson(self, json):
         id = json["id"]
         owner = json["owner"]
+        time = json["time"]
 
-        return Share(id=id, owner=owner)
+        return Share(id=id, owner=owner,time=time)
