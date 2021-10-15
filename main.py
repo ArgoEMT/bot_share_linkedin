f85fd44949fab2a2705565e7da04f2d313013d8a
Thomas <thomas.pouget@live.fr>
2021-10-15 18:00:56 +0200

    chore:main modification

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from api.user_api import *
 
-me = get_me()
-print(me.firstName)
-print(me.lastName)
-print(me.id)
+def main () :
+    me = get_me()
+    print(me.firstName)
+    print(me.lastName)
+    print(me.id)
+
+if __name__ == '__main__':
+    main()
